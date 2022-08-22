b25b521f683253884935537e7fb26aff0ae35f62
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-08-22 16:24:00 +0200

    temp voice functionality

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -4,7 +4,13 @@
  * Copyright (C) safeoasis.xyz team and contributors
  */
 
+'use strict'; // https://www.w3schools.com/js/js_strict.asp
+
 const { PermissionFlagsBits, ChannelType } = moduleRequire('discord.js');
+
+const ignored = JSON.parse(process.env.TMP_VOICE_CHANNEL_IGNORE);
+const lobby = process.env.TMP_VOICE_CHANNEL_LOBBY;
+const category = process.env.TMP_VOICE_CHANNEL_CATEGORY;
 
 // event when the botsession gets invalid
 module.exports = async (bot, oldState, newState) => {
@@ -12,44 +18,41 @@ module.exports = async (bot, oldState, newState) => {
     let selfMember = await oldState.guild.members.fetch(bot.user.id);
     if (!selfMember.permissions.has('MANAGE_CHANNELS')) return;
 
-    // var lobby = bot.configs.general.channels.tempvoice_lobby;
-    // var category = bot.configs.general.channels.tempvoice_category;
+    if (ignored.includes(oldState?.channel?.id)) return;
+    if (ignored.includes(newState?.channel?.id)) return;
 
-    // if (bot.configs.general.channels.tempvoice_ignore.includes(oldState?.channel?.id)) return;
-    // if (bot.configs.general.channels.tempvoice_ignore.includes(newState?.channel?.id)) return;
+    if (newState.channel && newState.channel.parent) {
+        if (newState.channel.id != lobby) return;
+        if (newState.channel.parent.id != category) return;
 
-    // if (newState.channel && newState.channel.parent) {
-    //     if (newState.channel.id != lobby) return;
-    //     if (newState.channel.parent.id != category) return;
+        let channel = await newState.member.guild.channels.create({
+            name: newState.member.displayName,
+            reason: 'new temp voice channels',
+            type: ChannelType.GuildVoice,
+            permissionOverwrites: [
+                {
+                    id: newState.member.id,
+                    allow: [PermissionFlagsBits.ManageChannels]
+                }
+            ]
+        });
 
-    //     let channel = await newState.member.guild.channels.create({
-    //         name: newState.member.displayName,
-    //         reason: 'new tempvoice channels',
-    //         type: ChannelType.GuildVoice,
-    //         permissionOverwrites: [
-    //             {
-    //                 id: newState.member.id,
-    //                 allow: [PermissionFlagsBits.ManageChannels]
-    //             }
-    //         ]
-    //     });
-
-    //     channel.setParent(category);
-    //     channel.setUserLimit(6);
-    //     newState.member.voice.setChannel(channel);
-    //     bot.db.insertAsync('temp_voice', { channel: channel.id, owner: newState.member.id, updates: 0 });
-    //     return;
-    // }
-    // if (oldState.channel.parent.id != category) return;
-    // bot.channels.cache.get(category).children.cache.forEach((channel) => {
-    //     if (bot.configs.channels.tempvoice_ignore.includes(channel?.id)) return;
-    //     if (channel.type == ChannelType.GuildVoice) {
-    //         if (channel.id != lobby) {
-    //             if (channel.members.size < 1) {
-    //                 channel.delete('making room for new tempvoice channels');
-    //                 bot.db.deleteAsync('temp_voice', { channel: channel.id });
-    //             }
-    //         }
-    //     }
-    // });
+        channel.setParent(category);
+        channel.setUserLimit(6);
+        newState.member.voice.setChannel(channel);
+        bot.db.insertAsync('temp_voice', { channel: channel.id, owner: newState.member.id, updates: 0 });
+        return;
+    }
+    if (oldState.channel.parent.id != category) return;
+    bot.channels.cache.get(category).children.cache.forEach((channel) => {
+        if (ignored.includes(channel?.id)) return;
+        if (channel.type == ChannelType.GuildVoice) {
+            if (channel.id != lobby) {
+                if (channel.members.size < 1) {
+                    channel.delete('making room for new temp voice channels');
+                    bot.db.deleteAsync('temp_voice', { channel: channel.id });
+                }
+            }
+        }
+    });
 };
