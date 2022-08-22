95d3f7aa3b5655ee6e7bb67b605edd5fcd04be47
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-08-22 12:56:51 +0200

    add message event

---
+++ events/messageCreate.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) LuciferMorningstarDev <contact@lucifer-morningstar.dev>
+ * Copyright (c) safeoasis.xyz <contact@safeoasis.xyz>
+ * Copyright (C) safeoasis.xyz team and contributors
+ */
+
+'use strict'; // https://www.w3schools.com/js/js_strict.asp
+
+module.exports = async (bot, message) => {};
