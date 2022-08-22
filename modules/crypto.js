41aea0fd49265afe4158214f27525b36a477733c
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-08-22 12:52:03 +0200

    add crypto methods

---
+++ modules/crypto.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) LuciferMorningstarDev <contact@lucifer-morningstar.dev>
+ * Copyright (c) safeoasis.xyz <contact@safeoasis.xyz>
+ * Copyright (C) safeoasis.xyz team and contributors
+ */
+
+'use strict'; // https://www.w3schools.com/js/js_strict.asp
+
+const crypto = require('node:crypto');
+
+const algorithm = 'aes-256-ctr';
+const secretKey = process.env.CRYPT_SECRET;
+
+const encrypt = (text) => {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+    return {
+        iv: iv.toString('hex'),
+        content: encrypted.toString('hex')
+    };
+};
+
+const decrypt = (hash) => {
+    const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(hash.iv, 'hex'));
+    const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash.content, 'hex')), decipher.final()]);
+    return decrpyted.toString();
+};
+
+module.exports = {
+    encrypt,
+    decrypt
+};
